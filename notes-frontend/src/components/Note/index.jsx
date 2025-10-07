ab6ad3e3fe4b93d8e9461bfa507946d14b7859b3
Enzo Santoro <ez.santoro@gmail.com>
2025-10-07 09:17:56 -0300

    Implementando a Criação de Anotações

--- notes-frontend/src/components/Note/index.jsx
+++ notes-frontend/src/components/Note/index.jsx
@@ -1,10 +1,21 @@
+import axios from "axios";
 import "./index.css";
 
 export default function Note(props) {
+  const deletarNote = () => {
+    axios
+      .delete(`http://localhost:8000/api/notes/${props.id}/`)
+      .then(() => props.loadNotes())
+      .catch((error) => console.log(error));
+  };
+
   return (
     <div className="card">
       <h3 className="card-title">{props.title}</h3>
       <div className="card-content">{props.children}</div>
+      <button className="btn btn-delete" onClick={deletarNote}>
+        Deletar
+      </button>
     </div>
   );
 }
